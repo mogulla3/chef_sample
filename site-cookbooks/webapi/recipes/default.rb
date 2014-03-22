249ba00f487235ee7f1149b6db898d9ed94ee4ad
Hiroki Sanpei <hs.iarn3@gmail.com>
2014-03-23 00:59:09 +0900

    first commit

---
+++ site-cookbooks/webapi/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: webapi
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Install MySQL
+
+
+# Install PHP
+
+
+# Install Apache
+
+
+# Install memcached
+
+
