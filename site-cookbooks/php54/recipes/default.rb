181ba75f966d62fbe2d864975d17e4799860bcdf
Hiroki Sanpei <hs.iarn3@gmail.com>
2014-03-23 16:42:32 +0900

    Add php54 cookbook

---
+++ site-cookbooks/php54/recipes/default.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: php54
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+# remote_file: リモートサーバにあるファイルをhttp経由で取得し、指定した場所に保存する
+# only_if: 評価結果が真の時に実行する
+# not_if: 評価結果が偽の時に実行する
+
+# xml2-config
+pkgs = %w(gcc make zlib-devel readline-devel libxml2-devel openssl-devel curl-devel bzip2-devel)
+
+pkgs.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+package "libmcrypt-devel" do
+  version "2.5.8-9.el6"
+  action :install
+  options "--enablerepo=epel"
+end
+
+filename = "php-5.4.0.tar.bz2"
+remote_uri = "http://museum.php.net/php5/php-5.4.0.tar.bz2";
+
+remote_file "/usr/src/#{filename}" do
+  source "#{remote_uri}"
+end
+
+bash "install_php54" do
+  not_if "which php"
+  cwd "/usr/src"
+
+  code <<-EOL
+    tar jxf #{filename}
+    cd php-5.4.0
+    ./configure --prefix=/usr --enable-maintainer-zts --with-pear --with-config-file-path=/etc --with-readline --with-mcrypt --with-zlib -enable-mbstring --with-curl --with-bz2 --enable-zip --enable-sockets --enable-sysvsem --enable-sysvshm --with-mhash --with-pcre-regex --with-gettext --enable-bcmath --enable-libxml --enable-json --with-openssl --enable-pcntl
+    make && make test && make install
+  EOL
+end
