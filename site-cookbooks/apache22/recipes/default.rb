72882055925f03361c29b3d1f8a85bf066b775de
Hiroki Sanpei <hs.iarn3@gmail.com>
2014-03-23 12:25:27 +0900

    Add apache22 cookbooks

---
+++ site-cookbooks/apache22/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: apache22
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Install
+package 'httpd' do
+  version '2.2.15-29.el6.centos'
+  action :install
+end
+
+# Setup
+
